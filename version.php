13e769b1e645c153808f1198ba49d97539377f6a
Benjamin Walker <benjaminwalker@catalyst-au.net>
2025-10-24 12:05:20 +1000

    Improve handling of missing grade items

--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_encoded';
-$plugin->version = 20250100900;
+$plugin->version = 20250102400;
 $plugin->requires = 2022112800;
 $plugin->supported = [401, 405];
