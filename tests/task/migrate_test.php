bc99080679bc75cf5e35084bc9fb668330a23bde
Niko Hoogeveen <niko.hoogeveen@gmail.com>
2026-06-08 12:41:23 -0400

    Issue 51: Fixed failing unit tests for Moodle 5.0+

--- tests/task/migrate_test.php
+++ tests/task/migrate_test.php
@@ -63,9 +63,8 @@ final class migrate_test extends advanced_testcase {
     ): void {
         global $DB;
 
-        $this->setOutputCallback(function ($output) {
-            // Ignore output.
-        });
+        // Ignore output.
+        ob_start();
 
         $generator = self::getDataGenerator();
         $creator = $generator->get_plugin_generator($component);
@@ -145,6 +144,8 @@ final class migrate_test extends advanced_testcase {
                 );
             }
         }
+        // Ignore output.
+        ob_end_clean();
     }
 
     /**
